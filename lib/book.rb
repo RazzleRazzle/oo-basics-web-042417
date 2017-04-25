23d44f345f4c3f6ef3da9d75a7afaf0b6e70a8cf
Jake Musich <makejusich@gmail.com>
2017-04-25 19:07:21 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
 
+  def initialize(title) #initialize method sets it up upon creation
+    @title = title
+  end
 
+  def title #getter
+    @title
+  end
+
+  def author=(author) #setter
+    @author = author
+  end
+
+  def author #getter
+    @author
+  end
+
+  def page_count=(num) #sets, receiving value assigned to instance variable
+    @page_count = num
+  end
+
+  def page_count #gets, prints, the value held by instance variable
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+end
